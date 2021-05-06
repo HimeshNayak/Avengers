a500109e9265182674253780920cf00b4c790952
HimeshNayak <himeshnayak015@gmail.com>
2021-05-06 14:23:08 +0530

    Added firebase and deployed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// @dart=2.9
+
 import 'package:flutter/material.dart';
 import 'homePage.dart';
 
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Avengers Rexora',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
